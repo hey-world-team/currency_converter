5657c555a5a87c10c5e7a5271f18cda9e00400c9
Codomiddle <oleg.freydenzon@gmail.com>
2022-04-28 19:59:03 +0200

    Class in Class was safety cleared

--- src/main/java/com/example/currency_converter/rest/controller.java
+++ src/main/java/com/example/currency_converter/rest/controller.java
@@ -1,15 +1,13 @@
 package com.example.currency_converter.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-public class controller {
+
 
     @RestController
-    public class RestAnnotatedController {
+    class RestAnnotatedController {
 
 
         }
-    }
+
 
