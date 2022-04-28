ec1059f1412df1f1b28eaba56085c23aff3b618a
Codomiddle <oleg.freydenzon@gmail.com>
2022-04-28 18:49:43 +0200

    Project Created and Class @RestController created

---
+++ src/main/java/com/example/currency_converter/rest/controller.java
@@ -0,0 +1,15 @@
+package com.example.currency_converter.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+public class controller {
+
+    @RestController
+    public class RestAnnotatedController {
+
+
+        }
+    }
+
