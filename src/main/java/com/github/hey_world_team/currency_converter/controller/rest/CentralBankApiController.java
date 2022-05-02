c7ecade42a05084bad896672ed6e49685489b24d
artemnoskov <artem.noskov@gmail.com>
2022-05-02 20:26:09 +0500

    anoskov_task_2 Added methods "getCurrencies" and "writeToFile" to CentralBankApiController Class for  getting xml and providing restapi

--- src/main/java/com/github/hey_world_team/currency_converter/controller/rest/CentralBankApiController.java
+++ src/main/java/com/github/hey_world_team/currency_converter/controller/rest/CentralBankApiController.java
@@ -1,7 +1,34 @@
 package com.github.hey_world_team.currency_converter.controller.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 @RestController
 public class CentralBankApiController {
+    @GetMapping(value = "/foreignCurrencies")
+    public String getCurrencies() throws IOException {
+        String link = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=02/03/2002";
+        RestTemplate restTemplate = new RestTemplate();
+        String currenciesXml = restTemplate.getForObject(link, String.class);
+        writeToFile(currenciesXml);
+        return currenciesXml;
+    }
+
+    public void writeToFile(String p) throws IOException {
+        File currencyFile = new File("./src/main/resources/foreign_currencies.xml");
+        try (FileOutputStream outputStream = new FileOutputStream(currencyFile, false)) {
+            byte[] strToBytes = p.getBytes();
+            outputStream.write(strToBytes);
+        } catch (IOException ex) {
+            Logger.getLogger(CentralBankApiController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
