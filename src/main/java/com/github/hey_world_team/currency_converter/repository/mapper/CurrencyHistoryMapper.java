ebf1e3c57e8b3fc3ed17147592e80486bbee22ed
Rassl Durdyev <durdyevruslan1@gmail.com>
2023-07-02 19:18:29 +0530

    added history_mapper for repository

--- src/main/java/com/github/hey_world_team/currency_converter/repository/mapper/CurrencyHistoryMapper.java
+++ src/main/java/com/github/hey_world_team/currency_converter/repository/mapper/CurrencyHistoryMapper.java
@@ -1,2 +1,26 @@
-package com.github.hey_world_team.currency_converter.repository.mapper;public class CurrencyHistoryMapper {
-}
+package com.github.hey_world_team.currency_converter.repository.mapper;
+
+import com.github.hey_world_team.currency_converter.model.CurrencyConversionHistory;
+import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.RowMapper;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class CurrencyHistoryMapper implements RowMapper<CurrencyConversionHistory> {
+
+
+        @Override
+        public CurrencyConversionHistory mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new CurrencyConversionHistory(
+            rs.getLong("id"),
+            rs.getDate("conversionDate"),
+            rs.getString("inputCurrency"),
+            rs.getBigDecimal("inputAmount"),
+            rs.getString("outputCurrency"),
+            rs.getBigDecimal("outputAmount"));
+        }
+    }
+
